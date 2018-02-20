57ceb5f6d05618dc9b86719acfb0edc2560f7c93
Alexander Lavrov <alexandr.lavrov@satellite-soft.ru>
2018-02-21 01:12:01 +0400

    Add type hinting

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from aiohttp import web
+from aiohttp.web_request import Request
+from aiohttp.web_response import Response
 
 
-async def index(request):
+async def index(request: Request) -> Response:
     return web.Response(text='Hello, world!')
