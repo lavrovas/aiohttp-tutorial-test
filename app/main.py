d838eeb664f3a570b73402229bca623c5c02be67
Alexander Lavrov <alexandr.lavrov@satellite-soft.ru>
2018-02-21 01:12:01 +0400

    Add type hinting

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from aiohttp import web
+from aiohttp.web_app import Application
 
 from app.routes import setup_routes
 
 
-def init():
+def init() -> Application:
     app = web.Application()
     setup_routes(app)
     return app
 
 
-def main():
+def main() -> None:
     app = init()
     web.run_app(app, host='127.0.0.1', port=8080)
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from aiohttp.web_app import Application
+
 from .views import index
 
 
-def setup_routes(app):
+def setup_routes(app: Application) -> None:
     app.router.add_get('/', index)
     app.router.add_get('/index', index)
