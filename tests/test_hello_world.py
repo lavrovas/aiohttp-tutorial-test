5cd1c6aa63137a2442828b7e39e8040739b04255
Alexander Lavrov <3430503+lavrovas@users.noreply.github.com>
2018-03-16 17:25:12 +0400

    Change tests

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,10 +1,16 @@
 async def test_get_root(cli):
     resp = await cli.get('/')
     assert resp.status == 200
-    assert await resp.text() == 'Hello, world!'
+
+    text = await resp.text()
+    assert 'Home Page - Microblog' in text
+    assert 'Hello, Miguel!' in text
 
 
 async def test_get_index(cli):
     resp = await cli.get('/index')
     assert resp.status == 200
-    assert await resp.text() == 'Hello, world!'
+
+    text = await resp.text()
+    assert 'Home Page - Microblog' in text
+    assert 'Hello, Miguel!' in text
